4ef180e02813f54680918343f49f09b11dabe823
shomyich <220009@astanait.edu.kz>
2023-06-01 02:58:47 +0600

    This class represents a search algorithm for a weighted graph. It is a generic class, meaning it can be used with any type of vertex. The search algorithm is based on a breadth-first search (BFS) approach to determine whether a path exists between a source vertex and a destination vertex in the graph.
    The class contains the following components:
    
    - `graph`: An instance of the `WeightedGraph<Vertex>` class, representing the graph on which the search is performed.
    
    The class provides the following methods:
    
    - `Search(WeightedGraph<Vertex> graph)`: The constructor initializes the search algorithm with the specified graph.
    - `getGraph()`: This method returns the graph used for searching.
    - `doesPathExist(Vertex source, Vertex destination)`: This method determines whether a path exists between the source and destination vertices. It performs a BFS traversal of the graph, starting from the source vertex. It maintains a set of visited vertices and a queue for exploring neighboring vertices. It returns `true` if the destination vertex is reached during the traversal, indicating that a path exists. Otherwise, it returns `false`.
    
    Overall, this class provides the functionality to perform a path existence check in a weighted graph using a breadth-first search approach. It allows users to instantiate a search object with a specific graph and then use the `doesPathExist()` method to determine if a path exists between two vertices in the graph.

---
+++ src/Search.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Search<Vertex> {
+    private WeightedGraph<Vertex> graph;
+
+    public Search(WeightedGraph<Vertex> graph) {
+        this.graph = graph;  // Initializes the graph used for searching
+    }
+
+    public WeightedGraph<Vertex> getGraph() {
+        return graph;  // Returns the graph being used for searching
+    }
+
+    public boolean doesPathExist(Vertex source, Vertex destination) {
+        Set<Vertex> visited = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+
+        visited.add(source);
+        queue.offer(source);
+
+        while (!queue.isEmpty()) {
+            Vertex current = queue.poll();
+
+            if (current.equals(destination)) {
+                return true;  // Returns true if the destination vertex is reached
+            }
+
+            List<Edge<Vertex>> adjacentEdges = graph.getAdjacentEdges(current);
+            for (Edge<Vertex> edge : adjacentEdges) {
+                Vertex neighbor = edge.getDestination();
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.offer(neighbor);  // Adds the unvisited neighboring vertices to the queue
+                }
+            }
+        }
+
+        return false;  // Returns false if the destination vertex is not reachable
+    }
+}
